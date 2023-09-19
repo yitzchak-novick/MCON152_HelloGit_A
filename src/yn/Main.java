dcf834332b2cfd09b00d703ea17f114128e0ba3d
yitzchak-novick <yitzchak.novick@touro.edu>
2023-09-19 16:23:18 -0400

    Updated main to show more information about message

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		MessageGenerator mg = new MessageGenerator();
 		String msg = mg.getMessage();
-		System.out.println(msg);
+		System.out.println("The message is: " + msg);
 
 	}
 
