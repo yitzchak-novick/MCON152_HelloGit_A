809b0a0fa78a5502aa464e0d816bb448e45596a2
Yitzchak Novick <yitzchak.novick@touro.edu>
2023-09-19 16:50:39 -0400

    foo and bar in messagegenerator.java

--- src/yn/MessageGenerator.java
+++ src/yn/MessageGenerator.java
@@ -4,4 +4,12 @@ public class MessageGenerator {
 	public String getMessage() {
 		return "Once upon a time";
 	}
+	
+	public void foo() {
+		System.out.println("foo");
+	}
+	
+	public void bar() {
+		System.out.println("bar");
+	}
 }
